a765a91473ae23cf1661ffb966511d715e3a6eea
JimmyNSI <jnsi4785@gmail.com>
2026-04-28 13:25:54 -0400

    container added

--- download_data.py
+++ download_data.py
@@ -6,7 +6,7 @@ connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 if not connection_string:
     raise RuntimeError("Falta AZURE_STORAGE_CONNECTION_STRING")
 
-container_name = "my-blob"
+container_name = "jimmy-data-raw"
 
 # Carpeta donde está este script/app.py
 download_path = os.path.dirname(os.path.abspath(__file__))
